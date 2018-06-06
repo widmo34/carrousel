5df93299874befaf1cbb21322d0f76d76b38616b
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-06-06 03:32:50 +0200

    google maps firts try

--- js/script.js
+++ js/script.js
@@ -49,5 +49,39 @@ f.on( 'scroll', function( progress ) {
 
 
 
+/* FUNCTION GOOGLE MAPS IS BELOW 
+
+window.initMap = function() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  var coords2 = {lat: -25.363, lng: 134.044};
+	var coords3 = {lat: -25.363, lng: 137.044};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+  document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  // var marker = new google.maps.Marker({position: uluru, map: map});
+  
+  var markerOne = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+
+ 
+
+
+
+
+  }
+
+
+
+  // MARKING NO A MAP 
+  var infos = document.getElementById('infos');
+*/
+
+
+
+
 
 
