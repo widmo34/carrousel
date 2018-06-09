1694edac922b05a03ddf29ebf3899d057a2227f4
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-06-10 00:40:52 +0200

    Finish google map

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ var output = document.querySelector('.main-carousel');
 var listItems = '';
 
 for(var i = 0; i < cities.length; i++){
-  console.log(cities);
   listItems += Mustache.render(templateItem, cities[i]);
 }
 
 output.insertAdjacentHTML('beforeend', listItems);
+
 
 
 
@@ -33,8 +33,15 @@ var button = document.querySelector('.btn');
 
 
 button.addEventListener('click', function(){
-      f.select(0);
+      f.select(1);
  })
+
+ flkty.once( 'eventName', function() {
+  console.log('eventName happened just once');
+});
+
+
+
 
 
 
@@ -44,82 +51,60 @@ var progressBar = document.querySelector('.progress-bar')
 f.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
+ 
 });
 
 
 
 
 /* FUNCTION GOOGLE MAPS IS BELOW */
-  
-
-
 window.initMap = function() {
 
-  // MARKING NO A MAP 
-var infos = document.getElementById('infos');
-  // The location of Uluru and two other places
-  var uluru = {lat: -25.344, lng: 131.036};
-  var coords2 = {lat: -25.363, lng: 134.044};
-  var coords3 = {lat: -25.363, lng: 137.044};
-  var sydney = {lat: -33.874237, lng: 151.198517};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-  document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-   var markerOne = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
- // The marker, poistioned near uluru 
-  var markerTwo = new google.maps.Marker({
-    position: coords2,
-    map: map
-  });
- // The marker, poistioned near uluru 
-  var markerThree = new google.maps.Marker({
-    position: coords3,
+  // The map, centered at Paris it's a first location when someone comes to a web page
+var newCoords = cities[0].coords;
+
+var map = new google.maps.Map(
+  document.getElementById('map'), {zoom: 4, center: newCoords});
+  // The marker, positioned at Paris
+    var markerOne = new google.maps.Marker({
+    position: newCoords,
     map: map
   });
 
-   // The marker, poistioned at Sydney 
-   var markerThree = new google.maps.Marker({
-    position: sydney,
-    map: map
-  });
+ var marker = [];
 
-
-
-
-
-  // I add eventListener when someone clicks a marker 
-  markerOne.addListener('click', function(){
-    infos.innerHTML = 'It"s a marker positioned at uluru';
-  
-  })
-
-  markerTwo.addListener('click', function(){
-    infos.innerHTML = 'It"s a marker positioned at coordinate 2';
-  
-  
-  })
-
-  markerThree.addListener('click', function(){
-    infos.innerHTML = 'It"s a marker positioned at  coordinate 3 ';
-   
-  
-  })
-  // It's a mthod which points position of Sydney a make a zoom of 10 
-  document.getElementById('center-map').addEventListener('click', function(event){
-    event.preventDefault();
-    
-    map.panTo(sydney);
-    map.setZoom(10);
-  });
-
-
-
-
+  for(var i = 0; i < cities.length; i++){
+      marker = new google.maps.Marker({
+      position: cities[i].coords,
+      map: map
+      });
 }
+
+  
+// this code moves a map depending on what slide is on screen 
+flkty.on( 'change', function( index ) {
+
+  if(index == 0 || index == 1){
+    map.panTo(cities[0].coords)
+  }else if(index == 2){
+    map.panTo(cities[1].coords)
+  }else if(index == 3){
+    map.panTo(cities[2].coords)
+  }else if(index == 4){
+    map.panTo(cities[3].coords)
+  }else if(index == 5){
+    map.panTo(cities[4].coords)
+  }
+  
+  
+  
+  });
+ 
+
+
+
+
+} // end of initMap
 
 
 
